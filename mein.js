3b3ccc5c5515c715245d64298d6f2c3e2953508a
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-22 11:57:12 +0500

    1-dars-js tugadi

--- mein.js
+++ mein.js
@@ -1,9 +1,10 @@
 let ism =prompt("ismingiz nma");
 let famiylangiz =prompt("famiylangiz nma");
-let yoshingiz =prompt("yoshingiz nechada");
+let yoshingiz = Number(prompt("yoshingiz nechada"));
 let mashinangiz =prompt("mashinangiz qanaqa");
 let ota =prompt("otangizni ismi nma");
 let ona =prompt("onangizni ismi nma");
 let yashashjoy =prompt("yashash joyingiz nomi");
 
-alert(ism + "\n" + famiylangiz + "\n" + yoshingiz  + "\n" +  mashinangiz + "\n" + ota + "\n" + ona + "\n" + yashashjoy)
+alert(ism + "\n" + famiylangiz + "\n" + yoshingiz  + "\n" +  mashinangiz + "\n" + ota + "\n" + ona + "\n" + yashashjoy);
+console.log(ism + "\n" + famiylangiz + "\n" + yoshingiz  + "\n" +  mashinangiz + "\n" + ota + "\n" + ona + "\n" + yashashjoy);
