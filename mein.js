cee2c9c86e81f35797a0bf3d7df772ed9a883841
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-22 11:40:16 +0500

    ozgartiruvchi elon qilindi

--- mein.js
+++ mein.js
@@ -1,3 +1,7 @@
 let ism =prompt("ismingiz nma");
 let famiylangiz =prompt("famiylangiz nma");
 let yoshingiz =prompt("yoshingiz nechada");
+let mashinangiz =prompt("mashinangiz qanaqa");
+let ota =prompt("otangizni ismi nma");
+let ona =prompt("onangizni ismi nma");
+let yashashjoy =("yashash joyingiz nomi");
